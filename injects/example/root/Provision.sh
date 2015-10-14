e066975cf7fd1f78e0a8d838216eb1da05cc825f
trojanspike <v3047-github@yahoo.co.uk>
2015-10-14 16:18:36 +0100

    exec script

--- injects/example/root/Provision.sh
+++ injects/example/root/Provision.sh
@@ -10,12 +10,13 @@
 ## give $USER PERMS to gpio
 
 IS_DEVICE=$1
+chmod +x /root/_Provisioning/*
 ### Run some logic or run a script
 if [ "$IS_DEVICE" == true ]; then
   echo "Doing for device"
   /root/_Provisioning/provision-device
 else
-  /root/_Provisioning/provision-qem
+  /root/_Provisioning/provision-qemu
 fi
 
 exit 0
