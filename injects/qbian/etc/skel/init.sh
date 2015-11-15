c9972d06eda62719a4b038dc4f6d5e7b4784cc4f
trojanspike <v3047-github@yahoo.co.uk>
2015-11-15 18:44:16 +0000

    qbian files

--- injects/qbian/etc/skel/init.sh
+++ injects/qbian/etc/skel/init.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-
 COMMAND=$1 # if [$COMMD = start | stop | reload]
+
+if [ ! -d ~/c9sdk ];then
+	c9-install && sudo reboot
+fi
+
 if [ "$COMMAND" == "start" ];then
-	cd ~/Server && npm start
+	cd ~/Server && npm run service-start && \
+	forever stop ~/c9sdk/server.js && \
+	forever start ~/c9sdk/server.js -p 8080 -l 0.0.0.0  -w ~/ --collab true --packed false -a :
 fi
 
 if [ "$COMMAND" == "reload" ];then
-	echo "Reload scripts"
+	cd ~/Server && npm run service-reload
 fi
 
 if [ "$COMMAND" == "stop" ];then
-	echo "Stopping scripts"
+	cd ~/Server && npm run service-stop
 fi
