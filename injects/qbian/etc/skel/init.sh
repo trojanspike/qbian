44ce57b1d68503460be77e21b1e9ce88dc36dc34
trojanspike <v3047-github@yahoo.co.uk>
2015-11-15 20:04:22 +0000

    init file to run ide when folder found & user installs it them selves - no auto run install

--- injects/qbian/etc/skel/init.sh
+++ injects/qbian/etc/skel/init.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 COMMAND=$1 # if [$COMMD = start | stop | reload]
 
-if [ ! -d ~/c9sdk ];then
-	c9-install && sudo reboot
-fi
 
 if [ "$COMMAND" == "start" ];then
-	cd ~/Server && npm run service-start && \
-	forever stop ~/c9sdk/server.js && \
-	forever start ~/c9sdk/server.js -p 8080 -l 0.0.0.0  -w ~/ --collab true --packed false -a :
+	if [ ! -d ~/c9sdk ];then
+		forever start ~/c9sdk/server.js -p 8080 -l 0.0.0.0  -w ~/ --collab true --packed false -a :
+	fi
 fi
 
 if [ "$COMMAND" == "reload" ];then
