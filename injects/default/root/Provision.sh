a76a035fcece71855efd3b18d771fc0749fb5bc4
trojanspike <v3047-github@yahoo.co.uk>
2015-11-30 17:13:50 +0000

    default provision update

--- injects/default/root/Provision.sh
+++ injects/default/root/Provision.sh
@@ -11,7 +11,8 @@ if [ "$IS_DEVICE" == true ]; then
 else
   echo "Running provision for qemu environment" && \
 	# active init.d qbian-provision
-	apt-get update && apt-get install vim -y && update-rc.d qbian-provision defaults
+	# TODO -> udev rules for /var/cache/man -> man:root
+	chmod 0755 /etc/ && chmod 0644 /etc/bash.bashrc && apt-get update && apt-get install vim -y && update-rc.d qbian-provision defaults
 fi
 
 exit 0
