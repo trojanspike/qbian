c05d4c958566813fe229cd08f6ecc78c1d169a2c
trojanspike <v3047-github@yahoo.co.uk>
2015-12-01 11:47:38 +0000

    cloud9 env

--- injects/qbian-c9/root/Provision.sh
+++ injects/qbian-c9/root/Provision.sh
@@ -5,7 +5,11 @@ chmod +x /root/_Provisioning/*
 ### Run some logic or run a script
 if [ "$IS_DEVICE" == true ]; then
   echo "Doing for device"
-  /root/_Provisioning/provision-device
+	if [ ! -f /qbianProvision/qbian-c9-qemu ];then
+		/root/_Provisioning/provision-qemu && touch /qbianProvision/qbian-c9-qemu && /root/_Provisioning/provision-device
+	else
+		/root/_Provisioning/provision-device
+	fi
 else
 	echo "Doing for qemu"
   /root/_Provisioning/provision-qemu
