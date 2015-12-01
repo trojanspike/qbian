2fcb87c031eeba38abac6bbc5694f97eb8f6fbee
trojanspike <v3047-github@yahoo.co.uk>
2015-12-01 12:11:31 +0000

    default provision script

--- injects/default/root/Provision.sh
+++ injects/default/root/Provision.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-IS_DEVICE=$1
-### Run some logic or run a script
-if [ "$IS_DEVICE" == true ]; then
-  echo "Running provision for device environment" && \
-	# Only upgrade when on device so that /boot can be updated & upgraded.
-	# Note that pi /boot dir doesn't get mounted the same in the emulator compared to the device
-  apt-get update && apt-get -f install -y && apt-get upgrade -y && apt-get -f install -y
-	# You could leave this until a later image if you want to.
-else
-  echo "Running provision for qemu environment" && \
-	# active init.d qbian-provision
-	# TODO -> udev rules for /var/cache/man -> man:root
-	chmod 0755 /etc/ && chmod 0644 /etc/bash.bashrc && apt-get update && apt-get install vim -y && update-rc.d qbian-provision defaults
-fi
+echo "Running provision for qemu environment" && \
+# active init.d qbian-provision
+# TODO ? -> udev rules for /var/cache/man -> man:root
+chmod 0755 /etc/ && chmod 0644 /etc/bash.bashrc && apt-get update && apt-get install vim -y && update-rc.d qbian-provision defaults && \
+touch /qbianProvision/default-qemu && touch /qbianProvision/default-device
 
 exit 0
