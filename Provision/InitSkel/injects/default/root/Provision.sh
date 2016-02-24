13e8dab7c2ae497552903a176562a6b9acb41cd0
trojanspike <v3047-github@yahoo.co.uk>
2016-02-24 14:19:02 +0000

    cli update

--- Provision/InitSkel/injects/default/root/Provision.sh
+++ Provision/InitSkel/injects/default/root/Provision.sh
@@ -3,7 +3,7 @@
 echo "Running provision for qemu environment" && \
 # active init.d qbian-provision
 # TODO ? -> udev rules for /var/cache/man -> man:root
-chmod 0755 /etc/ && chmod 0644 /etc/bash.bashrc && chown -R root:man /var/cache/man && apt-get update && apt-get install vim -y && update-rc.d qbian-provision defaults && \
+chmod 0755 /etc/ && chmod 0644 /etc/bash.bashrc && chown -R root:man /var/cache/man && apt-get update && apt-get install vim -y && \
 echo "Provision complete : restarting" && sleep 5 && reboot
 
 exit 0
